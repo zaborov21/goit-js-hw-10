b4b3cd4077a90e088be81e9c7b65c7e3f4f5c2d3
Andrii Zaborovskyi <zaborov21@gmail.com>
2023-09-17 21:22:01 +0200

    second commit

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,30 @@
-console.log("Hello world");
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+const rootRef = document.querySelector('.gallery');
+console.log(rootRef);
+
+function fetchPosts() {
+    fetch('https://api.thecatapi.com/v1/images/search?limit=10')
+        .then(response => response.json())
+        .then(data => {
+            const markup = data.map(data => {
+                return `
+                  
+                  <li class="gallery__item">
+                  <a class="gallery__link" href="${data.url}">
+                  <img class="gallery__image" src="${data.url}" alt="${data.id}}" width="386" height="240" />
+                  </a>
+                  </li>
+                       `
+            }).join("");
+
+            rootRef.insertAdjacentHTML('beforeend', markup);
+            const lightbox = new SimpleLightbox('.gallery a', {
+                captionsData: 'alt',
+                captionPosition: 'top',
+            });
+        });
+}
+
+fetchPosts();
